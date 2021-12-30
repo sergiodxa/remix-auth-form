ef75f0921c88b28a5515f19693d42ae37b9a5c31
Tom Rowe <theflyingcoder@hotmail.com>
2021-12-30 15:06:19 +1000

    Remember me checkbox
    If the user passes the field name of a "Remember me" checkbox, and subsequently the value is empty on success, expiry and maxAge are set to the current DateTime, and 0 respectively

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { AppLoadContext, SessionStorage } from "@remix-run/server-runtime";
+import { CookieSerializeOptions } from 'remix";
 import { AuthenticateOptions, Strategy } from "remix-auth";
+
+export interface FormAuthenticateOptions extends AuthenticateOptions {
+  /**
+   * The "Remember me" checkbox field name
+   *
+   * If set and the field is unchecked, the user session
+   * will not persist
+   */
+  rememberField?: string 
+}
 
 export interface FormStrategyVerifyParams {
   /**
@@ -25,7 +36,7 @@ export class FormStrategy<User> extends Strategy<
   async authenticate(
     request: Request,
     sessionStorage: SessionStorage,
-    options: AuthenticateOptions
+    options: FormAuthenticateOptions
   ): Promise<User> {
     let form = await request.formData();
 
@@ -38,5 +49,41 @@ export class FormStrategy<User> extends Strategy<
     }
 
     return this.success(user, request, sessionStorage, options);
+  }
+  
+  /**
+   * Returns the user data or throw a redirect to the successRedirect.
+   * @param user The user data to set in the session.
+   * @param session The session object to set the user in.
+   * @param options The form strategy options.
+   * @returns {Promise<User>} The user data.
+   * @throws {Response} If the successRedirect is set, it will redirect to it.
+   */
+  protected async success(
+    user: User,
+    request: Request,
+    sessionStorage: SessionStorage,
+    options: FormAuthenticateOptions
+  ): Promise<User> {
+    // if a successRedirect is not set, we return the user
+    if (!options.successRedirect) return user;
+
+    let session = await sessionStorage.getSession(
+      request.headers.get("Cookie")
+    );
+      
+    let cookieOptions: CookieSerializeOptions = {}
+      
+    if(!!options.rememberField && !(await request.formData()).get(options.rememberField)) {
+      cookieOptions.expiry = new Date(Date.now());
+      cookieOptions.maxAge = 0;
+    }
+
+    // if we do have a successRedirect, we redirect to it and set the user
+    // in the session sessionKey
+    session.set(options.sessionKey, user);
+    throw redirect(options.successRedirect, {
+      headers: { "Set-Cookie": await sessionStorage.commitSession(session, cookieOptions) },
+    });
   }
 }
